6fc634ec59c4c5c5613ba61f5bb0822b122dedf8
Kathy Moua <kathy@page-ed.org>
2022-04-06 23:35:48 -0500

    comment out post body

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -4,61 +4,61 @@ const { Post } = require('../models');
   {
     "title": "Doggie Meet at Park",
     "post_url": " ",
-    "body": "I am German Shepard. Meet me at a dog park to play fetch.",
+    // "body": "I am German Shepard. Meet me at a dog park to play fetch.",
     "user_id": "1"
   },
   {
     "title": "Snow Introduction",
     "post_url": " ",
-    "body": "I am a Persian Cat.",
+    // "body": "I am a Persian Cat.",
     "user_id": "2"
   },
   {
     "title": "Learn More about Flower Horn Fish",
     "post_url": " ",
-    "body": "Flowerhorn cichlids are ornamental aquarium fish noted for their vivid colors and the distinctively shaped heads for which they are named. Their head protuberance is formally called a nuchal hump. Like blood parrot cichlids, they are hybrids that exist in the wild only because of their release.",
+    // "body": "Flowerhorn cichlids are ornamental aquarium fish noted for their vivid colors and the distinctively shaped heads for which they are named. Their head protuberance is formally called a nuchal hump. Like blood parrot cichlids, they are hybrids that exist in the wild only because of their release.",
     "user_id": "3"
   },
   {
     "title": "What does Corn Snake Eat",
     "post_url": " ",
-    "body": "Mice should be your corn snake's most common food, but you can feed them other prey, such as suitably sized quails or rats.",
+    // "body": "Mice should be your corn snake's most common food, but you can feed them other prey, such as suitably sized quails or rats.",
     "user_id": "4"
   },
   {
     "title": "Tricks to teach a Yorkie",
     "post_url": " ",
-    "body": "Some tricks to teach a yorkie are sit, stay, lay down, speak, shake, and roll over.",
+    // "body": "Some tricks to teach a yorkie are sit, stay, lay down, speak, shake, and roll over.",
     "user_id": "5"
   },
   {
     "title": "Legends about Cats",
     "post_url": " ",
-    "body": "A legend about cats is that they can predict the weather.",
+    // "body": "A legend about cats is that they can predict the weather.",
     "user_id": "6"
   },
   {
     "title": "Polly Introduction",
     "post_url": " ",
-    "body": "I am a Cockatiel bird and I just learned how to say pretty bird.",
+    // "body": "I am a Cockatiel bird and I just learned how to say pretty bird.",
     "user_id": "3"
   },
   {
     "title": "Fall Photoshoot",
     "post_url": " ",
-    "body": "I am a french bulldog.",
+    // "body": "I am a french bulldog.",
     "user_id": "5"
   },
   {
     "title": "How Big Does a Gold Fish Get",
     "post_url": " ",
-    "body": "According to National Geographic, goldfish are usually around 7 to 16.1 inches long and weigh 0.2 to 0.6 pounds, but can top 5 pounds in the wild.",
+    // "body": "According to National Geographic, goldfish are usually around 7 to 16.1 inches long and weigh 0.2 to 0.6 pounds, but can top 5 pounds in the wild.",
     "user_id": "4"
   },
   {
     "title": "Love Birds",
     "post_url": " ",
-    "body": " Once there was a prince name Matt and a princess name Mary. They fell in love.",
+    // "body": " Once there was a prince name Matt and a princess name Mary. They fell in love.",
     "user_id": "1"
   }
 ];
